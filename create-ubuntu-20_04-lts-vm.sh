4c34b4a93b808f76e81e4fe7009028fee083601b
Brent Yates <brent.yates@swxtch.io>
2022-08-18 18:26:41 -0400

    Changed ubuntu scripts to create a single VM

--- create-ubuntu-20_04-lts-vm.sh
+++ create-ubuntu-20_04-lts-vm.sh
@@ -4,23 +4,25 @@
 # Copyright (C) 2022 swXtch.io
 # Permission to copy and modify is granted under the MIT license
 
-if [ $# -ne 8 ]; then
+if [ $# -ne 9 ]; then
     echo "Invalid number of arguments supplied."
-    echo "usage: script <rg> <vnetRG> <vnetName> <subnetCtrl> <subnetData> <vm_type> <admin> <key>"
+    echo "usage: script <name> <rg> <vnetRG> <vnetName> <subnetCtrl> <subnetData> <vm_type> <admin> <key>"
     exit -1
 fi
 
-RG=$1
-vnetRG=$2
-vnetName=$3
-subnetCtrl=$4
-subnetData=$5
-vm_type=$6
-admin=$7
-key=$8
+Name=$1
+RG=$2
+vnetRG=$3
+vnetName=$4
+subnetCtrl=$5
+subnetData=$6
+vm_type=$7
+admin=$8
+key=$9
 
 echo
 echo "Creating Stock Ubuntu 20.04 instances of type ${vm_type}"
+echo "  NAME: ${Name}"
 echo "  RG: ${RG}"
 echo "  VNET: ${vnetRG}/${vnetName}[${subnetCtrl}:${subnetData}]"
 echo "  ADMIN: ${admin}"
@@ -29,7 +31,7 @@ echo
 read -p "Press Y to continue:" -n 1 -r
 echo    # (optional) move to a new line
 if [[ $REPLY =~ ^[Yy]$ ]]; then
-    az deployment group create --resource-group ${RG} --template-file arm-ubuntu-20_04-lts.json --parameters vnetRG=${vnetRG} vnetName=${vnetName} subnetCtrl=${subnetCtrl} subnetData=${subnetData} virtualMachineSize=${vm_type} adminUsername=${admin} adminPublicKey="${key}"
+    az deployment group create --resource-group ${RG} --template-file arm-ubuntu-20_04-lts.json --parameters vmBaseName=${Name} vnetRG=${vnetRG} vnetName=${vnetName} subnetCtrl=${subnetCtrl} subnetData=${subnetData} virtualMachineSize=${vm_type} adminUsername=${admin} adminPublicKey="${key}"
 else
     echo "Aborting"
 fi
